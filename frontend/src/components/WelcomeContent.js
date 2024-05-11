17ffd514afdf19908c47b0aca562a7930bd3e48b
Michał Błaszczykowski <mblaszczykowski@warsawdynamics.com>
2024-05-12 00:10:39 +0200

    fix few warnings

--- frontend/src/components/WelcomeContent.js
+++ frontend/src/components/WelcomeContent.js
@@ -34,8 +34,8 @@ export default class WelcomeContent extends React.Component {
                                href="#">
                                 Get started
                                 <svg className="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24"
-                                     height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                     stroke-linecap="round" stroke-linejoin="round">
+                                     height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
+                                     strokeLinecap="round" strokeLinejoin="round">
                                     <path d="m9 18 6-6-6-6"/>
                                 </svg>
                             </a>
