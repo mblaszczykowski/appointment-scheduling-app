5cc794de47813ba8655382f59079c71096535ada
Michał Błaszczykowski <mblaszczykowski@warsawdynamics.com>
2024-05-18 22:06:32 +0200

    small changes

--- frontend/src/components/calendar/BookMeetingForm.js
+++ frontend/src/components/calendar/BookMeetingForm.js
@@ -31,7 +31,7 @@ export default function BookMeetingForm({ onSubmit, onCancel }) {
                 </div>
             </div>
             <div className="sm:col-span-4 mt-2">
-                <label htmlFor="notes" className="block text-sm font-medium leading-6 text-gray-900">Meeting Notes</label>
+                <label htmlFor="notes" className="block text-sm font-medium leading-6 text-gray-900">Meeting notes</label>
                 <div className="mt-1">
                     <textarea id="notes" name="notes" rows="4"
                               className="px-2 block resize-none w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
