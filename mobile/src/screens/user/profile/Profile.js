052d775716b243d1a6329830cf25d47158bc36b8
danielsiara <danielsiara111@wp.pl>
2024-06-16 08:30:54 +0200

    Personal Information added

--- mobile/src/screens/user/profile/Profile.js
+++ mobile/src/screens/user/profile/Profile.js
@@ -1,10 +1,10 @@
-import React, { useContext } from "react";
-import { ScrollView, Image, Text, TextInput, View } from "react-native";
+import React, { useContext, useState, useEffect } from "react";
+import { ScrollView, Image, Text, TextInput, View, TouchableOpacity } from "react-native";
 import * as Animatable from "react-native-animatable";
 import ProfileContext from "../../../context/ProfileContext";
 import Slider from "@react-native-community/slider";
 
-const Profile = () => {
+const Profile = ({ toggleEditing, isEditing }) => {
     const { profileState } = useContext(ProfileContext);
 
     const {
@@ -20,11 +20,40 @@ const Profile = () => {
         meetingDuration
     } = profileState.profile;
 
-    const daysOfWeek = availableDays.split(',');
+    const [localFirstname, setLocalFirstname] = useState(firstname);
+    const [localLastname, setLocalLastname] = useState(lastname);
+    const [localEmail, setLocalEmail] = useState(email);
+    const [localCalendarUrl, setLocalCalendarUrl] = useState(calendarUrl);
+    const [localMeetingLink, setLocalMeetingLink] = useState(meetingLink);
+    const [localAvailableFromHour, setLocalAvailableFromHour] = useState(availableFromHour.toString());
+    const [localAvailableToHour, setLocalAvailableToHour] = useState(availableToHour.toString());
+    const [localMeetingDuration, setLocalMeetingDuration] = useState(Number(meetingDuration));
+    const [localAvailableDays, setLocalAvailableDays] = useState(availableDays.split(','));
+
+    useEffect(() => {
+        setLocalFirstname(firstname);
+        setLocalLastname(lastname);
+        setLocalEmail(email);
+        setLocalCalendarUrl(calendarUrl);
+        setLocalMeetingLink(meetingLink);
+        setLocalAvailableFromHour(availableFromHour.toString());
+        setLocalAvailableToHour(availableToHour.toString());
+        setLocalMeetingDuration(Number(meetingDuration));
+        setLocalAvailableDays(availableDays.split(','));
+    }, [profileState]);
+
+    const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+    const toggleDay = (day) => {
+        const updatedDays = localAvailableDays.includes(day)
+            ? localAvailableDays.filter(d => d !== day)
+            : [...localAvailableDays, day];
+        setLocalAvailableDays(updatedDays);
+    };
 
     return (
         <Animatable.View animation="fadeInRight" className="flex-1 bg-[#3575EF]">
-            <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'center', alignItems: 'center' }} style={{ width: '100%' }}>
                 <View className="flex-grow justify-end items-center">
                     <Image
                         className="w-[140px] h-[140px] rounded-full"
@@ -32,40 +61,54 @@ const Profile = () => {
                     />
                 </View>
                 <View className="flex-grow justify-center items-center">
+                    <Text className="text-white bg-transparent mt-0.5">First Name</Text>
                     <TextInput
                         className="w-[300px] bg-[#ffffff4d] rounded-[25px] p-3 text-[16px] text-white m-3.5 font-light"
                         placeholder="First Name"
                         placeholderTextColor="#fff"
-                        value={firstname}
-                        editable={false}
+                        value={localFirstname}
+                        onChangeText={setLocalFirstname}
+                        editable={isEditing}
                     />
+
+                    <Text className="text-white bg-transparent mt-0.5">Last Name</Text>
                     <TextInput
                         className="w-[300px] bg-[#ffffff4d] rounded-[25px] p-3 text-[16px] text-white m-3.5 font-light"
                         placeholder="Last Name"
                         placeholderTextColor="#fff"
-                        value={lastname}
-                        editable={false}
+                        value={localLastname}
+                        onChangeText={setLocalLastname}
+                        editable={isEditing}
                     />
+
+                    <Text className="text-white bg-transparent mt-0.5">Email</Text>
                     <TextInput
                         className="w-[300px] bg-[#ffffff4d] rounded-[25px] p-3 text-[16px] text-white m-3.5 font-light"
                         placeholder="Email"
                         placeholderTextColor="#fff"
-                        value={email}
-                        editable={false}
+                        value={localEmail}
+                        onChangeText={setLocalEmail}
+                        editable={isEditing}
                     />
+
+                    <Text className="text-white bg-transparent mt-0.5">Calendar URL</Text>
                     <TextInput
                         className="w-[300px] bg-[#ffffff4d] rounded-[25px] p-3 text-[16px] text-white m-3.5 font-light"
                         placeholder="Calendar URL"
                         placeholderTextColor="#fff"
-                        value={calendarUrl}
-                        editable={false}
+                        value={localCalendarUrl}
+                        onChangeText={setLocalCalendarUrl}
+                        editable={isEditing}
                     />
+
+                    <Text className="text-white bg-transparent mt-0.5">Meeting Link</Text>
                     <TextInput
                         className="w-[300px] bg-[#ffffff4d] rounded-[25px] p-3 text-[16px] text-white m-3.5 font-light"
                         placeholder="Meeting Link"
                         placeholderTextColor="#fff"
-                        value={meetingLink}
-                        editable={false}
+                        value={localMeetingLink}
+                        onChangeText={setLocalMeetingLink}
+                        editable={isEditing}
                     />
 
                     <Text className="text-white bg-transparent mt-0.5">Available Hours</Text>
@@ -74,32 +117,44 @@ const Profile = () => {
                             className="flex-1 h-[50px] text-white bg-transparent text-center"
                             placeholder="From"
                             placeholderTextColor="#ffffffa0"
-                            value={availableFromHour.toString()}
-                            editable={false}
+                            value={localAvailableFromHour}
+                            onChangeText={setLocalAvailableFromHour}
+                            editable={isEditing}
                         />
                         <Text className="text-white mx-2">-</Text>
                         <TextInput
                             className="flex-1 h-[50px] text-white bg-transparent text-center"
                             placeholder="To"
                             placeholderTextColor="#ffffffa0"
-                            value={availableToHour.toString()}
-                            editable={false}
+                            value={localAvailableToHour}
+                            onChangeText={setLocalAvailableToHour}
+                            editable={isEditing}
                         />
                     </View>
 
                     <Text className="text-white bg-transparent mt-2">Available Days</Text>
                     <View className="w-[300px] bg-[#ffffff4d] rounded-[25px] p-3 m-2.5 flex-row flex-wrap justify-start">
-                        {daysOfWeek.map((day, index) => (
-                            <View key={index} className="p-2 m-1 rounded-lg bg-white text-black">
-                                <Text className="text-center text-black">
+                        {daysOfWeek.map((day) => (
+                            <TouchableOpacity
+                                key={day}
+                                className={`p-2 m-1 rounded-lg ${
+                                    localAvailableDays.includes(day) ? 'bg-blue-400' : 'bg-transparent'
+                                }`}
+                                onPress={() => isEditing && toggleDay(day)}
+                            >
+                                <Text
+                                    className={`text-center ${
+                                        localAvailableDays.includes(day) ? 'text-white' : 'text-black'
+                                    }`}
+                                >
                                     {day}
                                 </Text>
-                            </View>
+                            </TouchableOpacity>
                         ))}
                     </View>
 
+                    <Text className="text-white text-center mb-2">Meeting Duration: {localMeetingDuration} min</Text>
                     <View className="w-[300px] bg-[#ffffff4d] rounded-[25px] p-3 m-3.5">
-                        <Text className="text-white text-center mb-2">Meeting Duration: {meetingDuration} min</Text>
                         <Slider
                             style={{ width: '100%', height: 40 }}
                             minimumValue={15}
@@ -107,8 +162,9 @@ const Profile = () => {
                             step={15}
                             minimumTrackTintColor="#FFFFFF"
                             maximumTrackTintColor="#000000"
-                            value={Number(meetingDuration)}
-                            disabled={true}
+                            value={localMeetingDuration}
+                            onValueChange={setLocalMeetingDuration}
+                            disabled={!isEditing}
                         />
                     </View>
                 </View>
--- mobile/src/screens/user/profile/ProfileStack.js
+++ mobile/src/screens/user/profile/ProfileStack.js
@@ -1,18 +1,24 @@
-import React from "react";
-import {createStackNavigator} from "@react-navigation/stack";
+import React, { useState } from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 import Profile from "./Profile";
-import {Ionicons, MaterialCommunityIcons} from "@expo/vector-icons";
-import {useNavigation} from "@react-navigation/native";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 const Stack = createStackNavigator();
 
 const ProfileStack = () => {
     const navigation = useNavigation();
+    const [isEditing, setIsEditing] = useState(false);
+
+    const toggleEditing = () => {
+        setIsEditing(!isEditing);
+    };
+
     return (
         <Stack.Navigator>
             <Stack.Screen
                 name="ProfileStack"
-                component={Profile}
+                component={(props) => <Profile {...props} isEditing={isEditing} toggleEditing={toggleEditing} />}
                 options={{
                     title: "",
                     headerLeft: () => (
@@ -20,16 +26,17 @@ const ProfileStack = () => {
                             name="chevron-back-circle"
                             size={30}
                             color="white"
-                            style={{marginHorizontal: 20}}
+                            style={{ marginHorizontal: 20 }}
                             onPress={() => navigation.goBack()}
                         />
                     ),
                     headerRight: () => (
                         <MaterialCommunityIcons
-                            name="account-edit"
+                            name={isEditing ? "check" : "account-edit"}
                             size={30}
                             color="white"
-                            style={{marginHorizontal: 20}}
+                            style={{ marginHorizontal: 20 }}
+                            onPress={toggleEditing}
                         />
                     ),
                 }}
