a2dbe17fb2b3e4848ec3b1c567a78e7267e06237
Michał Błaszczykowski <mblaszczykowski@warsawdynamics.com>
2024-11-26 22:50:08 +0100

    add target

---
+++ backend/target/classes/appointments.sql
@@ -0,0 +1,68 @@
+
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id) VALUES (1, '2024-05-14 14:53:58.000000', 'booker1@gmail.com', 'booker1', '2024-05-14 14:54:21.000000', '2024-05-14 14:54:22.000000', 1);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id) VALUES (2, '2024-05-14 14:54:35.000000', 'booker2@gmail.com', 'booker2', '2024-05-13 00:00:00.000000', '2024-05-14 14:55:14.000000', 1);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id) VALUES (3, '2024-05-14 16:57:06.000000', 'gdhjsagdhjashgjag', 'Michał Błaszczykowski', '2024-05-17 18:00:00.000000', '2024-05-17 17:00:00.000000', 1);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id) VALUES (4, '2024-05-14 16:58:39.000000', 'adsadsadas', 'Misha Iereschenko', '2024-05-14 16:00:00.000000', '2024-05-14 15:30:00.000000', 1);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id) VALUES (5, '2024-05-14 17:06:28.000000', 'dasdasdsa', 'Daniel Siara', '2024-05-14 17:15:00.000000', '2024-05-14 17:00:00.000000', 1);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id) VALUES (6, '2024-05-14 17:10:00.000000', 'email1@gmail.com', 'John Doe', '2024-05-14 17:30:00.000000', '2024-05-14 17:00:00.000000', 1);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id) VALUES (7, '2024-05-14 17:15:00.000000', 'email2@gmail.com', 'Jane Smith', '2024-05-14 17:45:00.000000', '2024-05-14 17:15:00.000000', 1);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id) VALUES (8, '2024-05-14 17:20:00.000000', 'email3@gmail.com', 'Alice Johnson', '2024-05-14 18:00:00.000000', '2024-05-14 17:30:00.000000', 1);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id) VALUES (9, '2024-05-14 17:25:00.000000', 'email4@gmail.com', 'Bob Brown', '2024-05-14 18:30:00.000000', '2024-05-14 18:00:00.000000', 1);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id) VALUES (10, '2024-05-14 17:30:00.000000', 'email5@gmail.com', 'Eva Green', '2024-05-14 19:00:00.000000', '2024-05-14 18:30:00.000000', 1);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id) VALUES (11, '2024-05-14 17:35:00.000000', 'email6@gmail.com', 'Michael Johnson', '2024-05-14 19:30:00.000000', '2024-05-14 19:00:00.000000', 1);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id) VALUES (12, '2024-05-14 17:40:00.000000', 'email7@gmail.com', 'Sophia Smith', '2024-05-14 20:00:00.000000', '2024-05-14 19:30:00.000000', 1);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id) VALUES (13, '2024-05-14 17:45:00.000000', 'email8@gmail.com', 'James Wilson', '2024-05-14 20:30:00.000000', '2024-05-14 20:00:00.000000', 1);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id) VALUES (14, '2024-05-14 17:50:00.000000', 'email9@gmail.com', 'Emily Davis', '2024-05-14 21:00:00.000000', '2024-05-14 20:30:00.000000', 1);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id) VALUES (15, '2024-05-14 17:55:00.000000', 'email10@gmail.com', 'David Anderson', '2024-05-14 21:30:00.000000', '2024-05-14 21:00:00.000000', 1);
+
+
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (35, CURRENT_TIMESTAMP, 'john.smith@example.com', 'John Smith', '2024-05-20 18:00:00.000000', '2024-05-20 17:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (36, CURRENT_TIMESTAMP, 'emma.jones@example.com', 'Emma Jones', '2024-05-31 13:00:00.000000', '2024-05-31 12:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (37, CURRENT_TIMESTAMP, 'alexander.wang@example.com', 'Alexander Wang', '2024-05-21 14:00:00.000000', '2024-05-21 13:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (23, CURRENT_TIMESTAMP, 'sarah.miller@example.com', 'Sarah Miller', '2024-05-23 10:00:00.000000', '2024-05-23 09:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (24, CURRENT_TIMESTAMP, 'michael.johnson@example.com', 'Michael Johnson', '2024-05-29 16:00:00.000000', '2024-05-29 15:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (25, CURRENT_TIMESTAMP, 'olivia.davis@example.com', 'Olivia Davis', '2024-05-27 12:00:00.000000', '2024-05-27 11:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (26, CURRENT_TIMESTAMP, 'james.anderson@example.com', 'James Anderson', '2024-05-22 17:00:00.000000', '2024-05-22 16:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (27, CURRENT_TIMESTAMP, 'emily.thomas@example.com', 'Emily Thomas', '2024-05-18 09:00:00.000000', '2024-05-18 08:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (28, CURRENT_TIMESTAMP, 'daniel.wilson@example.com', 'Daniel Wilson', '2024-05-26 15:00:00.000000', '2024-05-26 14:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (29, CURRENT_TIMESTAMP, 'emma.brown@example.com', 'Emma Brown', '2024-05-28 11:00:00.000000', '2024-05-28 10:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (30, CURRENT_TIMESTAMP, 'william.jones@example.com', 'William Jones', '2024-05-19 14:00:00.000000', '2024-05-19 13:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (31, CURRENT_TIMESTAMP, 'sophia.williams@example.com', 'Sophia Williams', '2024-05-24 20:00:00.000000', '2024-05-24 19:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (32, CURRENT_TIMESTAMP, 'matthew.johnson@example.com', 'Matthew Johnson', '2024-05-30 08:00:00.000000', '2024-05-30 07:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (33, CURRENT_TIMESTAMP, 'olivia.martinez@example.com', 'Olivia Martinez', '2024-05-25 12:00:00.000000', '2024-05-25 11:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (34, CURRENT_TIMESTAMP, 'daniel.rodriguez@example.com', 'Daniel Rodriguez', '2024-05-17 18:00:00.000000', '2024-05-17 17:00:00.000000', 9, true);
+
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (46, CURRENT_TIMESTAMP, 'john.smith@example.com', 'John Smith', '2024-06-20 18:00:00.000000', '2024-06-20 17:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (47, CURRENT_TIMESTAMP, 'emma.jones@example.com', 'Emma Jones', '2024-06-30 13:00:00.000000', '2024-06-30 12:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (48, CURRENT_TIMESTAMP, 'alexander.wang@example.com', 'Alexander Wang', '2024-06-21 14:00:00.000000', '2024-06-21 13:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (49, CURRENT_TIMESTAMP, 'sarah.miller@example.com', 'Sarah Miller', '2024-06-23 10:00:00.000000', '2024-06-23 09:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (50, CURRENT_TIMESTAMP, 'michael.johnson@example.com', 'Michael Johnson', '2024-06-29 16:00:00.000000', '2024-06-29 15:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (51, CURRENT_TIMESTAMP, 'olivia.davis@example.com', 'Olivia Davis', '2024-06-27 12:00:00.000000', '2024-06-27 11:00:00.000000', 9, true);
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual) VALUES (52, CURRENT_TIMESTAMP, 'james.anderson@example.com', 'James Anderson', '2024-06-22 17:00:00.000000', '2024-06-22 16:00:00.000000', 9, true);
+
+
+
+-- Today's appointments (16 May 2024)
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual)
+VALUES
+    (16, '2024-04-14 14:53:58.000000', 'booker1@gmail.com', 'booker1', '2024-05-16 14:54:21.000000', '2024-05-16 14:54:22.000000', 1, true);
+
+-- Next days' appointments
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual)
+VALUES
+    (17, '2024-04-15 14:54:35.000000', 'booker2@gmail.com', 'booker2', '2024-05-17 14:54:21.000000', '2024-05-17 14:54:22.000000', 1, true);
+
+-- Next weeks' appointments
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual)
+VALUES
+    (18, '2024-04-16 16:57:06.000000', 'booker3@gmail.com', 'Michał Błaszczykowski', '2024-05-24 18:00:00.000000', '2024-05-24 17:00:00.000000', 1, true),
+    (19, '2024-06-14 16:58:39.000000', 'booker4@gmail.com', 'Misha Iereschenko', '2024-05-31 16:00:00.000000', '2024-05-31 15:30:00.000000', 1, true);
+
+-- Past appointments
+INSERT INTO public.appointments (id, booked_at, booker_email, booker_name, end_time, start_time, user_id, is_actual)
+VALUES
+    (20, '2024-06-18 17:06:28.000000', 'booker5@gmail.com', 'Daniel Siara', '2024-05-14 17:15:00.000000', '2024-05-14 17:00:00.000000', 1, true),
+    (21, '2024-06-19 17:10:00.000000', 'booker6@gmail.com', 'John Doe', '2024-05-13 17:30:00.000000', '2024-05-13 17:00:00.000000', 1, true),
+    (22, '2024-07-20 17:15:00.000000', 'booker7@gmail.com', 'Jane Smith', '2024-05-12 17:45:00.000000', '2024-05-12 17:15:00.000000', 1, true);
+
+
